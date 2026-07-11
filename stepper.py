fbbef3c26342c4242445789aad0f3bf078f224e5
mavavo <247800079+mavavo@users.noreply.github.com>
2026-07-11 23:39:44 +0200

    Add basic functions for stepper motor control via DRV 8825 and Raspberry Pi Pico WH

---
+++ stepper.py
@@ -0,0 +1,41 @@
+from machine import Pin
+from time import sleep_us
+
+STEP_PIN = 17  
+DIR_PIN = 16   
+EN_PIN = 20   
+
+en_pin = Pin(EN_PIN, Pin.OUT, value=1)   # value = 1 means driver disables motor (requires connection of ENABLE pin at driver and one GPIO at Pico), 0 means always enabled
+dir_pin = Pin(DIR_PIN, Pin.OUT, value=0)
+step_pin = Pin(STEP_PIN, Pin.OUT, value=0)
+
+def enable(state=True):
+    en_pin.value(0 if state else 1)
+
+def set_direction(clockwise=True):
+    dir_pin.value(1 if clockwise else 0)
+    sleep_us(5)  # > 650 ns setup time needed
+
+def step_once(pulse_us=10, pause_us=10):
+    step_pin.on()
+    sleep_us(pulse_us)   # signal on 1 for at least 1.9 µs at the step pin 
+    step_pin.off()
+    sleep_us(pause_us)   # signal on 0 for at least 1.9 µs at the step pin
+
+def move_steps(steps, speed_sps=200, clockwise=True):
+
+    set_direction(clockwise)
+
+    full_period = (1/speed_sps) * 10**6         # µs/step
+    half_period = int(full_period/2)            # µs/half_step
+
+    enable(True)
+    sleep_us(500)
+
+    try:
+        for _ in range(steps):
+            step_once(half_period, half_period)
+    finally:
+        step_pin.off()
+        enable(False)
+        sleep_us(500)
