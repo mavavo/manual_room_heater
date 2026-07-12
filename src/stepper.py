7077ee5db0e7cc9300cf182a3fbe6780a08828b3
mavavo <247800079+mavavo@users.noreply.github.com>
2026-07-13 01:52:10 +0200

    Ensure that speed and steps can't be equal to or below 0 and add comments

--- src/stepper.py
+++ src/stepper.py
@@ -1,9 +1,10 @@
 from machine import Pin
 from time import sleep_us
 
-STEP_PIN = 17  
-DIR_PIN = 16   
-EN_PIN = 20   
+# pins on pico:
+STEP_PIN = 17  # assigned physical pin: 22
+DIR_PIN = 16   # assigned physical pin: 21
+EN_PIN = 20    # ENABLE pin, assigned physical pin: 26
 
 en_pin = Pin(EN_PIN, Pin.OUT, value=1)   # value = 1 means driver disables motor (requires connection of ENABLE pin at driver and one GPIO at Pico), 0 means always enabled
 dir_pin = Pin(DIR_PIN, Pin.OUT, value=0)
@@ -23,11 +24,19 @@ def step_once(pulse_us=10, pause_us=10):
     sleep_us(pause_us)   # signal on 0 for at least 1.9 µs at the step pin
 
 def move_steps(steps, speed_sps=200, clockwise=True):
+    'Moves the motor in the given direction by the given number of stepts'
+    if steps <= 0:
+        return
 
     set_direction(clockwise)
 
+    if speed_sps <= 0:
+        speed_sps = 100
+
     full_period = (1/speed_sps) * 10**6         # µs/step
     half_period = int(full_period/2)            # µs/half_step
+    if half_period < 5:   # avoid pulses below 1.9 µs plus some buffer
+        half_period = 5  
 
     enable(True)
     sleep_us(500)
