e10110697109fe481d052fe43d3cdf9af1b22d06
mavavo <247800079+mavavo@users.noreply.github.com>
2026-07-14 23:10:53 +0200

    Add function to set the time on the Pico via NTP

--- src/wlan.py
+++ src/wlan.py
@@ -1,6 +1,7 @@
 import network
 import time
 from hidden import My_SSID, My_PASSWORD
+import ntptime
 
 SSID = My_SSID
 PASSWORD = My_PASSWORD
@@ -8,7 +9,7 @@ PASSWORD = My_PASSWORD
 wlan = network.WLAN(network.WLAN.IF_STA)
 
 def connect_wlan(timeout=15):
-    'Connects to configured wlan network and returns the wlan interface'
+    'Connects to configured wlan network and returns the wlan interface.'
     network.country('DE')
     wlan.active(True)
 
@@ -20,6 +21,12 @@ def connect_wlan(timeout=15):
             if time.ticks_diff(time.ticks_ms(), start) > timeout * 1000:
                 raise RuntimeError("wlan timeout")
             time.sleep_ms(200)
+    return wlan
+
+def sync_times_via_ntp():
+    'Synchronizes the RTC and UTC via NTP and returns wlan interface.'
+    wlan = connect_wlan()
+    ntptime.settime()   # Gets UTC from internet and sets RTC (real time clock) of Pico to this UTC (coordinated universal time) 
     return wlan
 
 def disconnect_wlan():
